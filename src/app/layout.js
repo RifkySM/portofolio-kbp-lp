e5a1169d00b5dd4bbdd81df4ad9907f57c690074
Fenty Solihah Gitlab <fentysolihahgitlab@gmail.com>
2025-04-16 08:48:07 +0700

    topbar design

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,11 @@ import Topbar from "@/components/topbar"
 import { Inter } from "next/font/google"
 import "./globals.css"
 
-// Opsional: Gunakan font system atau custom font
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Nama Website Anda",
-  description: "Deskripsi website Anda",
+  title: "KBPayuk",
+  description: "KBPayuk Website",
 }
 
 export default function RootLayout({ children }) {

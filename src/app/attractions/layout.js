ec5f2411c330e5e64986aff618cece15e19ab1c5
Fenty Solihah Gitlab <fentysolihahgitlab@gmail.com>
2025-04-22 15:27:35 +0700

    Attractions Page design

---
+++ src/app/attractions/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "Attractions | KBPayuk",
+    description: "Temukan berbagai atraksi lezat di KBPayuk",
+  }
+  
+  export default function AttractionsLayout({ children }) {
+    return <>{children}</>
+  }

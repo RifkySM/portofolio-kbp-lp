d50ab7bbeb47a02af436923a45283e364e455441
Fenty Solihah Gitlab <fentysolihahgitlab@gmail.com>
2025-05-08 12:55:55 +0700

    KBPa Vacay Mode

---
+++ src/app/kbp-vacay-mode/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "KBPa Vacay Mode | KBPayuk",
+    description: "Temukan berbagai tempat wisata unik di KBPayuk",
+  }
+  
+  export default function VacayLayout({ children }) {
+    return <>{children}</>
+  }

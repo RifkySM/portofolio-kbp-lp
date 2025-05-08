88392a6498fd624e1985c50b849e1e50cfe05e9b
Fenty Solihah Gitlab <fentysolihahgitlab@gmail.com>
2025-05-08 09:55:27 +0700

    KBPa FoodTalk

---
+++ src/app/kbpa-foodtalk/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "KBPa Foodtalk | KBPayuk",
+    description: "Temukan berbagai kuliner lezat di KBPayuk",
+  }
+  
+  export default function FoodLayout({ children }) {
+    return <>{children}</>
+  }
